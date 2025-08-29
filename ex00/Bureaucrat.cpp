3539d393a57dee8851dde5ab756f14fdb37da1c4
akyoshid <akyoshid@student.42tokyo.jp>
2025-08-29 18:40:31 +0000

    [DONE] ex00

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,7 +6,69 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 12:33:21 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/07/27 12:33:22 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/08/29 18:37:06 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(const std::string& name, int grade)
+    : name(name), grade(grade) {
+    if (grade < 1)
+        throw GradeTooHighException();
+    else if (grade > 150)
+        throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& src)
+    : name(src.name), grade(src.grade) {
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& src) {
+    if (this != &src)
+        grade = src.grade;
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat() {
+}
+
+const std::string& Bureaucrat::getName() const {
+    return name;
+}
+
+const int& Bureaucrat::getGrade() const {
+    return grade;
+}
+
+void Bureaucrat::incrementGrade() {
+    if (grade > 1)
+        --grade;
+    else
+        throw GradeTooHighException();
+}
+
+void Bureaucrat::decrementGrade() {
+    if (grade < 150)
+        ++grade;
+    else
+        throw GradeTooLowException();
+
+}
+
+Bureaucrat::GradeTooHighException::GradeTooHighException()
+    : std::runtime_error("Bureaucrat: grade is too high") {
+}
+
+Bureaucrat::GradeTooLowException::GradeTooLowException()
+    : std::runtime_error("Bureaucrat: grade is too low") {
+}
+
+std::ostream& operator<<(std::ostream& lhs, const Bureaucrat& rhs) {
+    lhs << rhs.getName()
+        << ", bureaucrat grade " << rhs.getGrade() << ".";
+    return lhs;
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,36 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 12:33:23 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/07/27 12:33:24 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/08/29 15:26:26 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat {
+ public:
+    Bureaucrat(const std::string& name, int grade);
+    Bureaucrat(const Bureaucrat& src);
+    Bureaucrat& operator=(const Bureaucrat& src);
+    ~Bureaucrat();
+    const std::string& getName() const;
+    const int& getGrade() const;
+    void incrementGrade();
+    void decrementGrade();
+    class GradeTooHighException : public std::runtime_error {
+     public:
+        GradeTooHighException();
+    };
+    class GradeTooLowException : public std::runtime_error {
+     public:
+        GradeTooLowException();
+    };
+ private:
+    Bureaucrat(); // = delete;
+    const std::string name;
+    int grade;
+};
+
+std::ostream& operator<<(std::ostream& lhs, const Bureaucrat& rhs);
